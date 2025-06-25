408101bf8b017fe3e6bad946836ac5c7d0efc964
simran maran <simranmaran10@gmail.com>
2025-06-25 21:30:51 +0530

    done

---
+++ sorting/insertion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[n]; // Take array input
+    // Input array elements
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // Insertion Sort Logic
+    for(int i = 1; i < n; i++) {
+        int current = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
---
+++ string.cpp
@@ -0,0 +1,25 @@
+//Question no 1
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   string s ="Cybrom";
+//   reverse(s.begin(), s.end()); //Rverse The string
+//   cout<<s; 
+// }
+
+
+
+
+// //Question no 4
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   string s="C++ is a general-purpose programming language.";
+//   cout<<s.length()<<endl;
+//   cout<<s.size()<<endl;
+
+// }
+
+
+
+
