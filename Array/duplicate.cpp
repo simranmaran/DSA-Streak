26d575ec4e29d4462bbcc5ada437293fb4cc8dde
simran maran <simranmaran10@gmail.com>
2025-09-06 16:11:10 +0530

    done

---
+++ Array/duplicate.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements of array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Duplicate elements are: ";
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+                break; 
+            }
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
