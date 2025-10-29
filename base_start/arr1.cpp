f1dbb32606d5239687ce2a1bf7cf9015414636a3
simran maran <simranmaran10@gmail.com>
2025-10-29 21:44:13 +0530

    done

--- base_start/arr1.cpp
+++ base_start/arr1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int arr[10];
-  cout<<"array is successfully created";
+  int a=5 ;
+  cout<<"the address of a is"<<&a<<endl;
   return 0;
 }
